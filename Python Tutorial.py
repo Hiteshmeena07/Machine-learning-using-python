e7378b920c69683c9d70f4e5c842e68658702f7a
HITESH KUMAR MEENA <HMEENA040@GMAIL.COM>
2020-05-15 01:03:51 +0530

    Add files via upload

---
+++ Python Tutorial.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+#print any command
+print("this is python tutorial")
+
+
+# In[3]:
+
+
+#print variable
+student = "Hitesh"
+student
+
+
+# In[7]:
+
+
+#type
+a = 10
+type(a)
+
+
+# In[10]:
+
+
+#complex
+a1 = 3+4j
+a1
+
+type(a1)
+
+
+# In[17]:
+
+
+#Arthmatic operator
+# + _ / *
+a = 10
+b = 20
+print(a,b)
+
+c =a+b
+d = a-b
+e = a*b
+f = a/b
+print (c)
+print(d)
+print(e)
+print(f)
+
+
+# In[20]:
+
+
+#length of string 
+Hitesh = "Hitesh"
+len(Hitesh)
+
+
+# In[ ]:
+
+
+
+
